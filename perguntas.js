efb20fe4ff3a87a8b2ab9bc0437887f748a62b5c
RENANPADILHA909 <renan.biscaia@escola.pr.gov.br>
2025-06-18 11:09:26 -0300

    Renan

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'futebol',
+    'Qual time foi campeão ano passado?',
+    'Botafogo'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital do Brasil?',
+    'Brasilia'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'futebol',
+    'Qual time foi campeão Brasileiro em 2012?',
+    'fluminense'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'geografia',
+    'Qual a capital da russia?',
+    'moscou'
 )
